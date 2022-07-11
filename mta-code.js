cdb7829cb0452dec932362d01625f32a7dc7aa61
dxsohel <108568976+dxsohel@users.noreply.github.com>
2022-07-11 13:42:49 +0600

    Create mta-code.js

---
+++ mta-code.js
@@ -0,0 +1,39 @@
+  <!-- Begin  
+                function create(form) {  
+                  if (form.input2.value != "") {  
+                   form.story.value +=" <b:if cond='data:blog.url == data:blog.homepageUrl'>   <meta name=\"description\" content=\"" +  
+                   form.input2.value + "\"/></b:if>\n";  
+                  }  
+                  if (form.input4.value != "") {  
+                   form.story.value +="<meta name=\"keywords\" content=\"" +  
+                   form.input4.value + "\"/>\n";  
+                  }  
+                  if (form.input5.value != "") {  
+                   form.story.value +="<meta name=\"author\" content=\"" +  
+                   form.input5.value + "\"/>\n";  
+                  }  
+                  if (form.input7.value != "") {  
+                   form.story.value +="<meta name=\"language\" content=\"" +  
+                   form.input7.value + "\"/>\n";  
+                  }  
+                  if (form.input10.value != "") {  
+                   form.story.value +="<meta name=\"country\" content=\"" +  
+                   form.input10.value + "\"/>\n";  
+                    
+                    
+                   
+                    }  
+                  if (form.input10.value != "") {  
+                   form.story.value +="<meta property=\"fb:admins\" content=\"" +  
+                   form.input10.value + "\"/>\n";  
+                    
+                    
+                   
+                    
+                  }  
+                  if (form.input15.value != "") {  
+                   form.story.value +="<meta content='general' name='rating'/><meta name=\"robots\" content=\"" +  
+                   form.input15.options[form.input15.selectedIndex].value + "\"/>\n";  
+                  }  
+                 }  
+                // Code Written By VipeBN.Blogspot.com -->
